1c5466f22b430e938dc488f3146624d77786f796
Muhammad Zunoon Ali <zunnoon2006@gmail.com>
2025-06-29 16:38:23 +0500

    structure
    update

--- routes/gpaRoutes.js
+++
@@ -1,8 +0,0 @@
-// // routes/gpaRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const { getGPA } = require('../controllers/gpaController');
-
-// router.get('/', getGPA);
-
-// module.exports = router;
